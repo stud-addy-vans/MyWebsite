e215e9bfaa4f9f7a6026af177b7bc9634954b993
stud-addy-vans <aditya.22scse1010958@galgotiasuniversity.edu.in>
2025-05-31 11:31:33 +0530

    Update the Portfolio images and clean up unused files

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-  strings: ["Frontend Developer", "Web Designer", "UI/UX Designer"],
+  strings: ["Web Developer", "Web Designer", "UI/UX Designer", "Programmer"],
   typeSpeed: 100,
   backSpeed: 60,
   loop: true,
